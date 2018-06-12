c04cf88053e59171f0c930bec8c44024834a6bd9
ChieftainY2k <ChieftainY2k@gmail.com>
2018-06-12 21:18:20 +0200

    Kerberos-io installation in a docker container

---
+++ kerberos-deb/autoremoval.sh
@@ -0,0 +1,7 @@
+partition=/dev/root
+imagedir=/etc/opt/kerberosio/capture/
+if [[ $(df -h | grep $partition | head -1 | awk -F' ' '{ print $5/1 }' | tr ['%'] ["0"]) -gt 90 ]];
+then
+    echo "Cleaning disk"
+    find $imagedir -type f | sort | head -n 100 | xargs -r rm -rf;
+fi;
