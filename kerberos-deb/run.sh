dbd5da8de7a0dba2a8b9a415a84fb62a337083fc
ChieftainY2k <ChieftainY2k@gmail.com>
2018-06-14 12:06:01 +0200

    Crobtab jobs

--- kerberos-deb/run.sh
+++ kerberos-deb/run.sh
@@ -1,11 +1,13 @@
 service php7.0-fpm restart
 service nginx restart
 
-#service kerberosio restart
-
 # Fix permissions
 chmod -R 777 /etc/opt/kerberosio/config
 
+# Init cron
+cron &
+
+# Init machinery
 while sleep 3; do
     echo "Starting kerberos-io machinery..."
     kerberosio
