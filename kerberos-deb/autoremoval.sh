ba83d4ab8b29f6402e65afb8ec6739b8af5b443e
ChieftainY2k <ChieftainY2k@gmail.com>
2018-06-16 22:14:52 +0200

    Use files size instead of free disk space

--- kerberos-deb/autoremoval.sh
+++ kerberos-deb/autoremoval.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 
+#thresholds for cleanup policies
+maximumAllowedSpaceTakenKb=600000 # how much space we allow files to take
+
 DATE=`date '+%Y-%m-%d %H:%M:%S'`
 imagedir=/etc/opt/kerberosio/capture/
 partition=$(df $imagedir | awk '/^\/dev/ {print $1}')
-minimumAcceptableSpaceKb=700000
 
 #TODO optimize this
-
 usedPercent=$(df -h | grep $partition | head -1 | awk -F' ' '{ print $5/1 }' | tr ['%'] ["0"])
-kbytesAvailable=$(df --sync $imagedir | tail -1 | awk '{print $4}')
+spaceAvailableKb=$(df --sync $imagedir | tail -1 | awk '{print $4}')
 filesCount=$(find $imagedir| wc -l)
-filesSize=$(du -h $imagedir | tail -1 | awk '{print $1}')
-echo "[$DATE] partition $partition for $imagedir is used in $usedPercent percent ($kbytesAvailable kb available), capture dir has $filesCount files (using $filesSize in total)"
+totalFilesSizeKb=$(du $imagedir | tail -1 | awk '{print $1}')
+echo "[$DATE] partition $partition for $imagedir is used in $usedPercent percent ($spaceAvailableKb kb available), capture dir has $filesCount files (using $totalFilesSizeKb kb in total)"
 
-#while [ $usedPercent -gt 90 ]
-while [ $kbytesAvailable -lt $minimumAcceptableSpaceKb ]
+while [ $totalFilesSizeKb -gt $maximumAllowedSpaceTakenKb ]
 do
-    echo "[$DATE] running low on disk space, removing some of the oldest files in $imagedir ..."
+    echo "[$DATE] cleaning up, removing some oldest files in $imagedir ..."
     find $imagedir -type f | sort | head -n 100 | xargs -r rm -rf;
 
     usedPercent=$(df -h | grep $partition | head -1 | awk -F' ' '{ print $5/1 }' | tr ['%'] ["0"])
     bytesAvailable=$(df --sync $imagedir | tail -1 | awk '{print $4}')
     filesCount=$(find $imagedir| wc -l)
-    filesSize=$(du -h $imagedir | tail -1 | awk '{print $1}')
-    echo "[$DATE] partition $partition for $imagedir is used in $usedPercent percent ($kbytesAvailable kb available), capture dir has $filesCount files (using $filesSize in total)"
+    totalFilesSizeKb=$(du $imagedir | tail -1 | awk '{print $1}')
+    echo "[$DATE] partition $partition for $imagedir is used in $usedPercent percent ($spaceAvailableKb kb available), capture dir has $filesCount files (using $totalFilesSizeKb kb in total)"
+    sleep 3
 done
 
-echo "[$DATE] Removing old h264 files..."
+echo "[$DATE] removing old temporary h264 files."
 tmpreaper -v 24h /etc/opt/kerberosio/h264/
-echo "[$DATE] Old h264 files removed."
+
 
