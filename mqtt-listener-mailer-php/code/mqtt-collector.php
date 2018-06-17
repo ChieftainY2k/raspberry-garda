b34d8edc986f349e79594c7b5949425ea2521178
ChieftainY2k <ChieftainY2k@gmail.com>
2018-06-17 14:27:02 +0200

    Composer lock listener stub

--- mqtt-listener-mailer-php/code/mqtt-collector.php
+++ mqtt-listener-mailer-php/code/mqtt-collector.php
@@ -5,43 +5,41 @@
 
 require('vendor/autoload.php');
 
-///**
-// * MQTT message processor
-// * @param string $topic
-// * @param string $message
-// */
-//function processMqttMessage($topic, $message)
-//{
-//    echo "[" . date("Y-m-d H:i:s") . "] received topic: " . $topic . " , message: " . $message . "\n";
-//}
-//
-//echo "[" . date("Y-m-d H:i:s") . "] starting mqtt php listener\n";
-//
-//$server = "mqtt-server";     // change if necessary
-//$port = 1883;                     // change if necessary
-//$username = "";                   // set your username
-//$password = "";                   // set your password
-//$client_id = "listener-mailer-php-".uniqid(""); // make sure this is unique for connecting to sever - you could use uniqid()
-//
-//$mqtt = new \Bluerhinos\phpMQTT($server, $port, $client_id);
-//if (!$mqtt->connect(true, NULL, $username, $password)) {
-//    throw new \Exception("Cannot connect to the MQTT server.");
-//}
-//
-//$mqtt->debug = true;
-//
-////subscribe to all topics
-//$topics['kios/detection/motion'] = [
-//    "qos" => 2,
-//    "function" => "processMqttMessage"
-//];
-//$mqtt->subscribe($topics, 0);
-//
-////main loop
-//while ($mqtt->proc()) {
-//}
-//
-//$mqtt->close();
-//
+echo "[" . date("Y-m-d H:i:s") . "] starting the listener.\n";
+$client = new Mosquitto\Client("mqtt-listener-mailer-php-" . uniqid(""));
+$client->onConnect('connect');
+$client->onDisconnect('disconnect');
+$client->onSubscribe('subscribe');
+$client->onMessage('message');
+$client->connect("mqtt-server", 1883, 60);
+$client->subscribe('#', 2);
 
+
+while (true) {
+    $client->loop();
+    sleep(2);
+}
+
+$client->disconnect();
+unset($client);
+
+function connect($responseCode, $responseMessage)
+{
+    echo "[" . date("Y-m-d H:i:s") . "] connected, got code $responseCode , message '$responseMessage'\n";
+}
+
+function subscribe()
+{
+    echo "[" . date("Y-m-d H:i:s") . "] subscribed to a topic\n";
+}
+
+function message(Mosquitto\Message $message)
+{
+    echo "[" . date("Y-m-d H:i:s") . "] received topic " . $message->topic . " with payload: " . $message->payload . "\n";
+}
+
+function disconnect()
+{
+    echo "[" . date("Y-m-d H:i:s") . "] disconnected\n";
+}
 
