57ecf4e6550556d4461b63b6270e81d66e589da8
ChieftainY2k <ChieftainY2k@gmail.com>
2018-06-19 01:59:38 +0200

    Cleanup

--- services/mqtt-server/run.sh
+++ services/mqtt-server/run.sh
@@ -3,16 +3,13 @@
 #modify local config
 sed -i '/^log_dest/s/^/#/g' /etc/mosquitto/mosquitto.conf
 
-#prepare bridge configuration if enabled
+#prepare bridge configuration if bridge is enabled
 if [ "$KD_MQTT_BRIDGE_ENABLED" == "1" ]
 then
 
     configFile=/etc/mosquitto/conf.d/bridge.conf
-    echo "Create the bridge config $configFile"
+    echo "Creating the bridge config $configFile"
     echo "" > ${configFile}
-
-    # create the mosquitto local config for bridge to remote mqtt server
-
     echo "connection bridge-to-therabithia" >> ${configFile}
     echo "address $KD_MQTT_BRIDGE_REMOTE_HOST:$KD_MQTT_BRIDGE_REMOTE_PORT" >> ${configFile}
     echo "remote_clientid $KD_SYSTEM_NAME" >> ${configFile}
@@ -20,16 +17,6 @@ then
     echo "remote_password $KD_MQTT_BRIDGE_REMOTE_PASSWORD" >> ${configFile}
     echo "topic # out 1 \"\" $KD_MQTT_BRIDGE_REMOTE_OUT_TOPIC_PREFIX/" >> ${configFile}
     echo "topic # in 1" remote/ \"\">> ${configFile}
-#    echo "topic # both 1 remote $KD_MQTT_BRIDGE_REMOTE_OUT_TOPIC_PREFIX">> ${configFile}
-#    echo "topic # both 1">> ${configFile}
-
-    #bridge_cafile /path/to/losant/LosantRootCA.crt
-    #cleansession true
-    #try_private false
-    #bridge_attempt_unsubscribe false
-    #notifications false
-
-#    cat ${configFile}
 
 fi
 
