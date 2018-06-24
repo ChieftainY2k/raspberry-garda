5fed8039b175738b3cdbb10893961fce8f3e72bb
ChieftainY2k <ChieftainY2k@gmail.com>
2018-06-24 21:44:50 +0200

    Refactoring

--- services/configurator/code/configurator.php
+++ services/configurator/code/configurator.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Simple UI to update config and reload docker containers.
+ *
+ * @FIXME This is just a proof of concept, refactor it!
+ */
 
 require(__DIR__ . "/vendor/autoload.php");
 
@@ -6,7 +11,20 @@ use Docker\Docker;
 
 class Configurator
 {
+
+    /**
+     * Show simple UI
+     */
     static function showUI()
+    {
+        echo "Main menu:<hr>";
+        echo "[<a href='/reloadContainers'>reload containers</a>]";
+    }
+
+    /**
+     * Reload all containers except for this one with configurator
+     */
+    static function reloadContainers()
     {
         echo "Reloading containers... <hr>";
 
@@ -23,8 +41,20 @@ class Configurator
             }
 
         }
+
+        echo "<hr>Containers successfully reloaded.";
+
     }
 }
 
-echo $_SERVER['REQUEST_URI'] . "<br>";
-Configurator::showUI();
+//simple routing, wimple web interface
+$requestUrl = $_SERVER['REQUEST_URI'];
+//echo "Current URL: ". $requestUrl . "<br>";
+switch ($requestUrl) {
+    case "/reloadContainers":
+        Configurator::reloadContainers();
+        break;
+    default:
+        Configurator::showUI();
+}
+
--- services/configurator/run.sh
+++ services/configurator/run.sh
@@ -2,23 +2,7 @@
 # Workaround: preserve the environment for cron process
 printenv | grep -v "no_proxy" >> /etc/environment
 
-#echo "Starting services..."
-#
-#service nginx restart
-#
-## Install external libraries
-#echo "Installing libraries..."
-#cd /code
-#composer install
-#
-#
-#sleep infinity
-
-
-# init the log file
-#touch /listener.log
-
-# run  the listener forever
+# run the simple PHP process to act as web interface
 while sleep 3; do
     echo "Starting the configurator server..."
     php -S 0.0.0.0:80 /code/configurator.php
