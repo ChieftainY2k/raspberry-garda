3c3ad3dc285462c38301a468ac9bcb7bff7e97cf
ChieftainY2k <ChieftainY2k@gmail.com>
2018-06-25 22:01:48 +0200

    autoremove updated

--- services/kerberos/autoremoval.sh
+++ services/kerberos/autoremoval.sh
@@ -19,6 +19,8 @@ partition=$(df $imagedir | awk '/^\/dev/ {print $1}')
 
 
 usedPercent=$(df -h | grep $partition | head -1 | awk -F' ' '{ print $5/1 }' | tr ['%'] ["0"])
+spaceTotalKb=$(df --sync $imagedir | tail -1 | awk '{print $1}') # total space (free+used)
+spaceUsedKb=$(df --sync $imagedir | tail -1 | awk '{print $2}') # used space
 spaceAvailableKb=$(df --sync $imagedir | tail -1 | awk '{print $4}') # currently available free space on device
 filesCount=$(find $imagedir| wc -l) # number of captured files
 totalFilesSizeKb=$(du $imagedir | tail -1 | awk '{print $1}') # total size of captured files
@@ -26,20 +28,32 @@ logMessage "partition $partition for $imagedir is used in $usedPercent percent (
 
 #max allowed space for files:
 #maximumAllowedSpaceTakenKb=600000 # fixed = how much we allow files to take
-maximumAllowedSpaceTakenKb=$(($spaceAvailableKb-500000)) # dynamic = related to the overall free space on device
+#maximumAllowedSpaceTakenKb=$(($spaceAvailableKb-1500000)) # dynamic = related to the overall free space on device
+maximumAllowedSpaceTakenKb=$(($spaceAvailableKb-1500000)) # dynamic = related to the overall free space on device
+#logMessage "totalFilesSizeKb = $totalFilesSizeKb"
+#logMessage "maximumAllowedSpaceTakenKb = $maximumAllowedSpaceTakenKb"
+#logMessage "spaceAvailableKb = $spaceAvailableKb"
 
 cleanupPerformed=0
-while [ $totalFilesSizeKb -gt $maximumAllowedSpaceTakenKb ]
+#while [ $totalFilesSizeKb -gt $maximumAllowedSpaceTakenKb ]
+while [ $spaceAvailableKb -lt 1500000 ]
 do
     logMessage "cleaning up, removing some oldest files in $imagedir ..."
     find $imagedir -type f | sort | head -n 100 | xargs -r rm -rf;
 
     usedPercent=$(df -h | grep $partition | head -1 | awk -F' ' '{ print $5/1 }' | tr ['%'] ["0"])
+    spaceTotalKb=$(df --sync $imagedir | tail -1 | awk '{print $1}') # total space (free+used)
+    spaceUsedKb=$(df --sync $imagedir | tail -1 | awk '{print $2}') # used space
     spaceAvailableKb=$(df --sync $imagedir | tail -1 | awk '{print $4}')
     filesCount=$(find $imagedir| wc -l)
     totalFilesSizeKb=$(du $imagedir | tail -1 | awk '{print $1}')
 
     logMessage "partition $partition for $imagedir is used in $usedPercent percent ($spaceAvailableKb kb available), capture dir has $filesCount files (using $totalFilesSizeKb kb in total)"
+
+    #logMessage "totalFilesSizeKb = $totalFilesSizeKb"
+    #logMessage "maximumAllowedSpaceTakenKb = $maximumAllowedSpaceTakenKb"
+    #logMessage "spaceAvailableKb = $spaceAvailableKb"
+
     sleep 3
 
     cleanupPerformed=1
