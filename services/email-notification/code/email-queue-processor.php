b67653c215a99647a52505e4d35963cf9d462a7f
ChieftainY2k <ChieftainY2k@gmail.com>
2018-07-01 16:07:07 +0200

    Email queue processor stub

---
+++ services/email-notification/code/email-queue-processor.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Queue processor to send all emails pending in the email queue
+ *
+ * This script takes each message from the queue and sends it via SMTP server
+ *
+ * @TODO this is just MVP/PoC, refactor it !
+ */
+
