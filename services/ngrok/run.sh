4211cf3976c795c806828e197c065450e7f3c7d8
ChieftainY2k <ChieftainY2k@gmail.com>
2019-01-08 14:39:47 +0100

    Ngrok service for remote to local tunnelling

---
+++ services/ngrok/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#load services configuration
+export $(grep -v '^#' /service-configs/services.conf | xargs -d '\n')
+
+if [[ "$KD_NGROK_ENABLED" == "1" ]]; then
+
+    while sleep 3; do
+        echo "Starting the NGROK tunnel client to kerberos server..."
+        /sbin/ngrok authtoken ${KD_NGROK_AUTHTOKEN}
+        /sbin/ngrok http kerberos:80
+        echo "NGROK tunnel client stopped, sleeping for a while..."
+        sleep 600
+    done
+
+else
+
+    echo "NGROK tunnel is DISABLED."
+
+fi
+
+
+
