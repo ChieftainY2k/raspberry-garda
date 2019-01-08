5664cea0045e60c31ee28d05705c5a465f156105
ChieftainY2k <ChieftainY2k@gmail.com>
2019-01-08 15:44:12 +0100

    ngrok log to std output

--- services/ngrok/run.sh
+++ services/ngrok/run.sh
@@ -6,9 +6,9 @@ export $(grep -v '^#' /service-configs/services.conf | xargs -d '\n')
 if [[ "$KD_NGROK_ENABLED" == "1" ]]; then
 
     while sleep 3; do
-        echo "Starting the NGROK tunnel client to kerberos server..."
+        echo "Starting the NGROK tunnel client to the local kerberos webserver..."
         /sbin/ngrok authtoken ${KD_NGROK_AUTHTOKEN}
-        /sbin/ngrok http kerberos:80
+        /sbin/ngrok http -log stdout kerberos:80
         echo "NGROK tunnel client stopped, sleeping for a while..."
         sleep 600
     done
