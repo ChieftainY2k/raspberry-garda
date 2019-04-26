541175aeb0f4fe40d917851e91f243bb610fbfbe
ChieftainY2k <ChieftainY2k@gmail.com>
2019-04-26 21:10:19 +0200

    refactoring, rpi-update updated

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #helper function
-logMessage()
+log_message()
 {
     LOGPREFIX="[$(date '+%Y-%m-%d %H:%M:%S')][install]"
     MESSAGE=$1
@@ -12,35 +12,35 @@ logMessage()
 check_errors()
 {
     EXITCODE=$1
-    if [ $EXITCODE -ne 0 ]; then
-        logMessage "ERROR: there were some errors, check the ouput for details, press ENTER to continue or Ctrl-C to abort."
+    if [[ ${EXITCODE} -ne 0 ]]; then
+        log_message "ERROR: there were some errors, check the ouput for details, press ENTER to continue or Ctrl-C to abort."
         read
     else
-        logMessage "OK, operation successfully completed."
+        log_message "OK, operation successfully completed."
     fi
 }
 
 ipAddress=$(ip route get 1 | awk '{print $NF;exit}')
 availableDiskSpaceKb=$(df | grep /dev/root | awk '{print $4/1}')
-logMessage "Starting installation. My IP address: $ipAddress , available disk space: $availableDiskSpaceKb kb."
+log_message "Starting installation. My IP address: $ipAddress , available disk space: $availableDiskSpaceKb kb."
 
 #logMessage "Updating the firmware..."
 #rpi-update
 #check_errors $?
 
-logMessage "Updating packages..."
+log_message "Updating packages..."
 sudo apt-get update -y
 check_errors $?
 sudo apt-get upgrade -y
 check_errors $?
 
-logMessage "Checking if camera module is enabled and taking sample picture..."
+log_message "Checking if camera module is enabled and taking sample picture..."
 raspistill -o /tmp/$(date +%s).jpg
 check_errors $?
 
 
 # Install some required packages first
-logMessage "Installing packages..."
+log_message "Installing packages..."
 sudo apt install -y \
      apt-transport-https ca-certificates \
      curl wget telnet gnupg2 software-properties-common \
@@ -49,7 +49,7 @@ sudo apt install -y \
 check_errors $?
 
 # Get the Docker signing key for packages
-logMessage "Installing docker..."
+log_message "Installing docker..."
 curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
 check_errors $?
 echo "deb [arch=armhf] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
@@ -65,12 +65,12 @@ check_errors $?
 sudo systemctl start docker
 check_errors $?
 
-logMessage "Installing docker-compose..."
+log_message "Installing docker-compose..."
 # Install Docker Compose from pip
 pip install docker-compose
 check_errors $?
 
-logMessage "Starting services..."
+log_message "Starting services..."
 # start the containers
 COMPOSE_HTTP_TIMEOUT=200 docker-compose up -d --remove-orphans
 check_errors $?
@@ -80,6 +80,6 @@ check_errors $?
 #check_errors $?
 
 availableDiskSpaceKb=$(df | grep /dev/root | awk '{print $4/1}')
-logMessage "Installation complete. My IP address: $ipAddress , available disk space: $availableDiskSpaceKb kb."
-logMessage "Run 'docker-compose logs -f' to see services logs."
+log_message "Installation complete. My IP address: $ipAddress , available disk space: $availableDiskSpaceKb kb."
+log_message "Run 'docker-compose logs -f' to see services logs."
 
