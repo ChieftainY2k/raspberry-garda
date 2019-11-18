939cd3d3ecd0b15ea0540496347768bc92f6ca7c
ChieftainY2k <ChieftainY2k@gmail.com>
2019-11-18 17:52:39 +0100

    Auth protected nginx proxy entry point

--- services/nginx/run.sh
+++ services/nginx/run.sh
@@ -2,7 +2,13 @@
 # Workaround: preserve the environment for cron process
 printenv | grep -v "no_proxy" >> /etc/environment
 
+#load services configuration
+export $(grep -v '^#' /service-configs/services.conf | xargs -d '\n')
+
 #sleep infinity
+
+# set global UI password
+htpasswd -cb /etc/nginx/.htpasswd "${KD_UI_USER}" "${KD_UI_PASSWORD}"
 
 while sleep 3; do
     echo "Starting nginx..."
