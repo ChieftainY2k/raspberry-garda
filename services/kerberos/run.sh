986d16d7fae7cbd8ae09419d864ebce0aff4c1a0
ChieftainY2k <ChieftainY2k@gmail.com>
2019-11-20 13:56:57 +0100

    Allow kerberos service to be disabled

--- services/kerberos/run.sh
+++ services/kerberos/run.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 # Workaround: preserve the environment for cron process
 printenv | grep -v "no_proxy" >> /etc/environment
@@ -5,7 +6,13 @@ printenv | grep -v "no_proxy" >> /etc/environment
 #load services configuration
 export $(grep -v '^#' /service-configs/services.conf | xargs -d '\n')
 
-echo "Starting kerberos-io service."
+if [[ "${KD_KERBEROS_ENABLED}" != "1" ]]; then
+    echo "NOTICE: kerberos service is DISABLED, going to sleep..."
+    sleep infinity
+    exit
+fi
+
+echo "Starting kerberos services..."
 
 service php7.0-fpm restart
 service nginx restart
@@ -22,7 +29,7 @@ rsyslogd &
 cron &
 
 # Init machinery
-while sleep 3; do
+while sleep 60; do
     echo "Starting kerberos-io machinery."
     kerberosio
     echo "Kerberos-io process terminated."
