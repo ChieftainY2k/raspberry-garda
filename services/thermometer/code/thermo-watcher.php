9caab3d6d15aae74025f7cf5a17d2d308f3a3762
ChieftainY2k <ChieftainY2k@gmail.com>
2019-11-24 23:06:33 +0100

    Thermometer stub

--- services/thermometer/code/thermo-watcher.php
+++ services/thermometer/code/thermo-watcher.php
@@ -7,17 +7,8 @@
  * @TODO this is just MVP/PoC, refactor it , use DI!
  */
 
-echo "[" . date("Y-m-d H:i:s") . "] Starting topics analyzer.\n";
+echo "[" . date("Y-m-d H:i:s") . "] Starting temp sensors watcher.\n";
 require(__DIR__ . "/bootstrap.php");
 
-//queue root path
-$healthReportsRootPath = "/data/health-reports";
-//email queue root path
-$emailQueuePath = "/data-email-notification/email-queues/default";
-$localCacheRootPath = "/data/cache";
-
-//init topics collector with the mqtt client
-$reportAnalyzer = new \SwarmWatcher\ReportAnalyzer($healthReportsRootPath, $emailQueuePath, $localCacheRootPath);
-$reportAnalyzer->sendNotificationsBasedOnReports();
 
 echo "[" . date("Y-m-d H:i:s") . "] finished.\n";
