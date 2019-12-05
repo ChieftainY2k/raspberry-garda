b66f376bc1d87936f65854f94388c858dec2e460
ChieftainY2k <ChieftainY2k@gmail.com>
2019-12-05 11:45:03 +0100

    Kerberos config in docker

---
+++ configs/kerberos/web/kerberos.php
@@ -0,0 +1,17 @@
+<?php return array (
+    'type' => 'basic',
+    'radius' => '1',
+    'installed' => true,
+    'users' =>
+        array (
+            0 =>
+                array (
+                    'id' => 1,
+                    'username' => 'root',
+                    'password' => 'root',
+                    'firstname' => 'Firstname',
+                    'secondname' => 'Lastname',
+                    'language' => 'en',
+                ),
+        ),
+);
--- services/kerberos/run.sh
+++ services/kerberos/run.sh
@@ -1,38 +1,86 @@
 #!/bin/bash
 
+#helper function
+log_message()
+{
+    LOGPREFIX="[$(date '+%Y-%m-%d %H:%M:%S')][kerberos]"
+    MESSAGE=$1
+    echo "$LOGPREFIX $MESSAGE"
+}
+
+#check for errors
+check_errors()
+{
+    local EXITCODE=$1
+    if [[ ${EXITCODE} -ne 0 ]]; then
+        log_message "ERROR: Exit code ${EXITCODE} , check the ouput for details."
+        exit 1
+    fi
+}
+
 # Workaround: preserve the environment for cron process
 printenv | grep -v "no_proxy" >> /etc/environment
 
 #load services configuration
 export $(grep -v '^#' /service-configs/services.conf | xargs -d '\n')
+check_errors $?
 
 if [[ "${KD_KERBEROS_ENABLED}" != "1" ]]; then
-    echo "NOTICE: KERBEROS service is DISABLED, going to sleep..."
+    log_message "NOTICE: KERBEROS service is DISABLED, going to sleep..."
     sleep infinity
     exit
 fi
 
-echo "Starting kerberos services..."
+log_message "starting the kerberos service..."
 
-service php7.0-fpm restart
-service nginx restart
-
-# Fix permissions
+# replace kerberos web config with a link to the dockerized one
+check_errors $?
+log_message "backing up config file..."
+mv /var/www/web/config/kerberos.php /var/www/web/config/kerberos.php.orig
+check_errors $?
+log_message "replacing config file..."
+ln -s /web-config-dist/kerberos.php /var/www/web/config/kerberos.php
+check_errors $?
+log_message "initializing configuration dir..."
 chmod -R 777 /etc/opt/kerberosio/config
+check_errors $?
+log_message "initializing session dir..."
+chmod -R 777 /var/www/web/storage/framework/sessions
+check_errors $?
+log_message "initializing configuration file..."
+chmod a+rw /var/www/web/config/kerberos.php
+check_errors $?
+
+#sleep infinity
+
+log_message "starting frontend app..."
+service php7.0-fpm restart
+check_errors $?
+log_message "starting nginx..."
+service nginx restart
+check_errors $?
+
 
 # Fix permissions & run the script
 chmod u+x /code/autoremoval.sh
+check_errors $?
 /code/autoremoval.sh
+check_errors $?
 
 # Init crontab and cron process
+log_message "starting syslog..."
 rsyslogd &
+check_errors $?
+log_message "starting cron..."
 cron &
+check_errors $?
 
 # Init machinery
-while sleep 60; do
-    echo "Starting kerberos-io machinery."
+while sleep 1; do
+    log_message "starting kerberos-io machinery..."
     kerberosio
-    echo "Kerberos-io process terminated."
+    log_message "kerberosio process terminated, sleeping and retrying..."
+    sleep 60
 done
 
 #sleep infinity
