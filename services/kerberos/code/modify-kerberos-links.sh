3b98031e1690218d751a8713239726b336959a53
ChieftainY2k <ChieftainY2k@gmail.com>
2020-05-06 16:37:59 +0200

    refactoring

--- services/kerberos/code/modify-kerberos-links.sh
+++ services/kerberos/code/modify-kerberos-links.sh
@@ -18,4 +18,36 @@ check_errors()
     fi
 }
 
-/var/www/web
+sed -i "s|Route::get('login|Route::get('kerberos/login|g" /var/www/web/routes/* /var/www/web/app/Providers/*
+sed -i "s|Route::post('login/login|Route::post('kerberos/login/login|g" /var/www/web/routes/* /var/www/web/app/Providers/*
+sed -i "s|Route::prefix('api|Route::prefix('kerberos/api|g" /var/www/web/routes/* /var/www/web/app/Providers/*
+sed -i "s|Route::get('users|Route::get('kerberos/users|g" /var/www/web/routes/* /var/www/web/app/Providers/*
+
+sed -i "s|Route::get('images|Route::get('kerberos/images|g" /var/www/web/routes/* /var/www/web/app/Providers/*
+sed -i "s|Route::get('system|Route::get('kerberos/system|g" /var/www/web/routes/* /var/www/web/app/Providers/*
+
+sed -i "s|Route::post('cloud|Route::post('kerberos/cloud|g" /var/www/web/routes/* /var/www/web/app/Providers/*
+
+sed -i "s|Route::get('name|Route::get('kerberos/name|g" /var/www/web/routes/* /var/www/web/app/Providers/*
+sed -i "s|Route::put('name|Route::put('kerberos/name|g" /var/www/web/routes/* /var/www/web/app/Providers/*
+
+sed -i "s|Route::get('condition|Route::get('kerberos/condition|g" /var/www/web/routes/* /var/www/web/app/Providers/*
+sed -i "s|Route::put('condition|Route::put('kerberos/condition|g" /var/www/web/routes/* /var/www/web/app/Providers/*
+
+
+#Route::get('
+#Route::get('/kerberos/
+#
+#Route::post('
+#Route::post('/kerberos/
+#
+#
+#{{URL::to('
+#{{URL::to('/kerberos/
+#
+#RouteServiceProvider.php
+#Route::prefix('api')
+#Route::prefix('/kerberos/api')
+#
+#$.get( _baseUrl + "/api/v1/system/kerberos", function(data)
+#$.get( _baseUrl + "/kerberos/api/v1/system/kerberos", function(data)
