a9ccf1c8af81c40cb600b5c82bd5f6f7e0cb43b8
ChieftainY2k <ChieftainY2k@gmail.com>
2020-05-06 15:44:57 +0200

    refactoring

---
+++ services/kerberos/code/modify-kerberos-links.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#helper function
+log_message()
+{
+    LOGPREFIX="[$(date '+%Y-%m-%d %H:%M:%S')][$(basename $0)]"
+    MESSAGE=$1
+    echo "$LOGPREFIX $MESSAGE"
+}
+
+#check for errors
+check_errors()
+{
+    local EXITCODE=$1
+    if [[ ${EXITCODE} -ne 0 ]]; then
+        log_message "ERROR: Exit code ${EXITCODE} , check the ouput for details, press ENTER to continue or Ctrl-C to abort."
+        exit 1
+    fi
+}
+
+/var/www/web
