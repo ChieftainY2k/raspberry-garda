fa02bdc792351b666e04c737e7ebd71e0ce11986
ChieftainY2k <ChieftainY2k@gmail.com>
2020-05-21 11:03:55 +0200

    make kerberos links scheme-relative

--- services/kerberos/resources/modify-kerberos-links.sh
+++ services/kerberos/resources/modify-kerberos-links.sh
@@ -18,8 +18,6 @@ check_errors()
     fi
 }
 
-#FILEDIRS="/var/www/web/routes/* /var/www/web/app/Providers/* /var/www/web/resources/views/*.php"
-
 log_message "updating kerberos code..."
 
 sed -i "s|Route::prefix('api|Route::prefix('kerberos/api|g" /var/www/web/app/Providers/RouteServiceProvider.php
@@ -29,54 +27,17 @@ sed -i "s|Route::get('kerberos//'|Route::get('kerberos/dashboard'|g" /var/www/we
 sed -i "s|Route::post('|Route::post('kerberos/|g" /var/www/web/routes/web.php
 sed -i "s|Route::put('|Route::put('kerberos/|g" /var/www/web/routes/web.php
 
-
-
-#find /var/www/web/resources/views -type f -exec sed -i -e 's|var _baseUrl = "{{URL::to(\x27/\x27)}}"|var _baseUrl = "{{URL::to(\x27/kerberos\x27)}}"|g' {} \;
 find /var/www/web/resources/views -type f -exec sed -i -e "s|URL::to('/|URL::to('/kerberos/|g" {} \;
+find /var/www/web/resources/views -type f -exec sed -i -e 's|_baseUrl = "http://|_baseUrl = "//|g' {} \;
 find /var/www/web/resources/views -type f -exec sed -i -e 's|url: "|url: "/kerberos|g' {} \;
 find /var/www/web/public/js -type f -exec sed -i -e 's|href="/api|href="/kerberos/api|g' {} \;
-
-#find /var/www/web/resources/views -type f -exec sed -i -e "s|URL::to('/kerberos/'|URL::to('/kerberos/dashboard'|g" {} \;
 
 find /var/www/web/app/Http/Controllers -type f -exec sed -i -e "s|URL::to('/|URL::to('/kerberos/|g" {} \;
 find /var/www/web/app/Http/Controllers -type f -exec sed -i -e "s|Redirect::to('|Redirect::to('kerberos/|g" {} \;
 find /var/www/web/app/Http/Middleware -type f -exec sed -i -e "s|redirect('|redirect('kerberos|g" {} \;
 find /var/www/web/app/Http/Middleware -type f -exec sed -i -e "s|redirect('kerberos/'|redirect('kerberos/dashboard'|g" {} \;
-#find /var/www/web/app/Http/ -type f -exec sed -i -e "s|redirect('/|redirect('/kerberos/|g" {} \;
 
 log_message "creating symbolic links..."
 ln -s /var/www/web/public /var/www/web/public/kerberos
 check_errors $?
 
-#sed -i "s|Route::get('login|Route::get('kerberos/login|g" /var/www/web/routes/* /var/www/web/app/Providers/*
-#sed -i "s|Route::post('login/login|Route::post('kerberos/login/login|g" /var/www/web/routes/* /var/www/web/app/Providers/*
-#sed -i "s|Route::prefix('api|Route::prefix('kerberos/api|g" /var/www/web/routes/* /var/www/web/app/Providers/*
-#sed -i "s|Route::get('users|Route::get('kerberos/users|g" /var/www/web/routes/* /var/www/web/app/Providers/*
-#sed -i "s|Route::post('users|Route::post('kerberos/users|g" /var/www/web/routes/* /var/www/web/app/Providers/*
-#sed -i "s|Route::get('images|Route::get('kerberos/images|g" /var/www/web/routes/* /var/www/web/app/Providers/*
-#sed -i "s|Route::get('system|Route::get('kerberos/system|g" /var/www/web/routes/* /var/www/web/app/Providers/*
-#sed -i "s|Route::post('cloud|Route::post('kerberos/cloud|g" /var/www/web/routes/* /var/www/web/app/Providers/*
-#sed -i "s|Route::get('name|Route::get('kerberos/name|g" /var/www/web/routes/* /var/www/web/app/Providers/*
-#sed -i "s|Route::put('name|Route::put('kerberos/name|g" /var/www/web/routes/* /var/www/web/app/Providers/*
-#sed -i "s|Route::get('condition|Route::get('kerberos/condition|g" /var/www/web/routes/* /var/www/web/app/Providers/*
-#sed -i "s|Route::put('condition|Route::put('kerberos/condition|g" /var/www/web/routes/* /var/www/web/app/Providers/*
-#sed -i "s|Route::get('condition|Route::get('kerberos/condition|g" /var/www/web/routes/* /var/www/web/app/Providers/*
-#sed -i "s|Route::put('condition|Route::put('kerberos/condition|g" /var/www/web/routes/* /var/www/web/app/Providers/*
-
-
-#Route::get('
-#Route::get('/kerberos/
-#
-#Route::post('
-#Route::post('/kerberos/
-#
-#
-#{{URL::to('
-#{{URL::to('/kerberos/
-#
-#RouteServiceProvider.php
-#Route::prefix('api')
-#Route::prefix('/kerberos/api')
-#
-#$.get( _baseUrl + "/api/v1/system/kerberos", function(data)
-#$.get( _baseUrl + "/kerberos/api/v1/system/kerberos", function(data)
