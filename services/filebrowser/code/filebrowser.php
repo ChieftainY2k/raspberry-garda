551284059802b077c5ef9dc80cb359b4d441d79d
ChieftainY2k <ChieftainY2k@gmail.com>
2020-08-31 13:58:41 +0200

    simple file browser

--- services/filebrowser/code/filebrowser.php
+++ services/filebrowser/code/filebrowser.php
@@ -1,1 +1,35 @@
-<?php phpinfo();
+<?php
+
+if (!empty($_GET['filename'])) {
+
+    if (!preg_match("/^[.a-z0-9_-]+$/i", $_GET['filename'])) {
+        throw new InvalidArgumentException("Malformed file name");
+    }
+
+    //stream file content
+    header("content-type: ".mime_content_type("/data-kerberosio-capture/".$_GET['filename'].""));
+    readfile("/data-kerberosio-capture/".$_GET['filename']);
+
+
+} else {
+
+    //show all files
+    echo "
+        <html>
+        <head>
+            <title>Filebrowser (" . htmlspecialchars(getenv("KD_SYSTEM_NAME")) . ")</title>
+        </head>
+        <body>
+    ";
+    $filesList = glob("/data-kerberosio-capture/*");
+    sort($filesList);
+    foreach ($filesList as $filename) {
+        echo "<a href='?filename=".htmlspecialchars(basename($filename))."'>".basename($filename)."</a><br>";
+    }
+    echo "
+        </body>
+        </html>
+    ";
+
+}
+?>
